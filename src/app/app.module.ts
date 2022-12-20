a10ea7328be8fc3a2941f251b4160a976de25cb4
Jett <jericochandra4@gmail.com>
2022-12-20 15:00:15 +0700

    Tugas 26

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserlistComponent } from './components/userlist/userlist.component';
+import { UsereditComponent } from './components/useredit/useredit.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NavBrandComponent } from './components/nav-brand/nav-brand.component';
+import { FormsModule } from '@angular/forms';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserlistComponent,
+    UsereditComponent,
+    LoginPageComponent,
+    NavBrandComponent,
+    DashboardPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
